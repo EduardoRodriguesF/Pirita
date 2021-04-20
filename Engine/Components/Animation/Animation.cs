d4d5163817c17e7d34624af7eeaee5367ce5d9ac
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-04-20 08:43:37 -0300

    feat: Animation

---
+++ Engine/Components/Animation/Animation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirita.Engine.Components.Animation {
+    public class Animation {
+        public Texture2D Texture { get; private set; }
+        public int CurrentFrame { get; set; }
+        public int FrameCount { get; private set; }
+        public int FrameHeight { get { return Texture.Height; } }
+        public float FrameSpeed { get; set; }
+        public int FrameWidth { get { return Texture.Width / FrameCount; } }
+        public bool isLooping { get; set; }
+
+        public Animation(Texture2D _texture, int _frameCount, float _frameSpeed) {
+            Texture = _texture;
+            FrameCount = _frameCount;
+            FrameSpeed = _frameSpeed;
+
+            isLooping = true;
+        }
+    }
+}
---
+++ Engine/Components/Animation/AnimationManager.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirita.Engine.Components.Animation {
+    public class AnimationManager {
+        private Animation _animation;
+        private float _timer;
+        public Vector2 Position { get; set; }
+        private int _flipX;
+
+        public AnimationManager(Animation animation) {
+            _animation = animation;
+        }
+
+        public void InvertX(bool flip) {
+            _flipX = flip ? 1 : 0;
+        }
+
+        public void Play(Animation animation) {
+            if (_animation == animation) return;
+
+            _animation = animation;
+            _animation.CurrentFrame = 0;
+            _timer = 0;
+        }
+
+        public void Stop() {
+            _timer = 0f;
+            _animation.CurrentFrame = 0;
+        }
+
+        public void Update(GameTime gameTime) {
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer > _animation.FrameSpeed) {
+                _timer = 0f;
+                _animation.CurrentFrame++;
+
+                if (_animation.CurrentFrame >= _animation.FrameCount)
+                    _animation.CurrentFrame = 0;
+            }
+        }
+
+        public void Render(SpriteBatch spriteBatch, float _opacity) {
+            spriteBatch.Draw(_animation.Texture, Position,
+                new Rectangle(
+                    _animation.CurrentFrame * _animation.FrameWidth,
+                    0,
+                    _animation.FrameWidth,
+                    _animation.FrameHeight
+                ), Color.White * _opacity, 0, new Vector2(0, 0), new Vector2(1, 1), (SpriteEffects)_flipX, 0f);
+        }
+    }
+}
