86efb97824c9c14eedc3501dbf8eeb8705e94590
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-04-28 11:56:56 -0300

    feat(UI): Grid

---
+++ UI/Grid.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirita.UI {
+    public class Grid {
+        private GridBox[][] _boxes;
+
+        public Grid(int horizontalBoxes, int verticalBoxes, Viewport bounds) {
+            var width = bounds.Width / horizontalBoxes;
+            var height = bounds.Height / verticalBoxes;
+
+            for (var i = 0; i < horizontalBoxes; i++) {
+                for (var j = 0; j < verticalBoxes; j++) {
+                    var xPos = bounds.X + (width * i);
+                    var yPos = bounds.Y + (height * j);
+
+                    _boxes[i][j] = new GridBox(xPos, yPos, width, height);
+                }
+            }
+        }
+
+        public GridBox GetBox(int row, int column) {
+            return _boxes[row][column];
+        }
+
+        public void RenderBounds(SpriteBatch spriteBatch) {
+            foreach (GridBox[] row in _boxes) {
+                foreach (GridBox gridBox in row) {
+                    gridBox.RenderBounds(spriteBatch);
+                }
+            }
+        }
+    }
+}
---
+++ UI/GridBox.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirita.UI {
+    public class GridBox {
+        public int Width;
+        public int Height;
+        public Vector2 Position;
+
+        public GridBox(float x, float y, int width, int height) {
+            Width = width;
+            Height = height;
+            Position = new Vector2(x, y);
+        }
+
+        public void RenderBounds(SpriteBatch spriteBatch) {
+            var texture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+            var color = Color.Yellow;
+            var lineWidth = 1;
+
+            spriteBatch.Draw(texture, new Rectangle((int) Position.X, (int) Position.Y, lineWidth, Height + lineWidth), color);
+            spriteBatch.Draw(texture, new Rectangle((int)Position.X, (int)Position.Y, Width + lineWidth, lineWidth), color);
+            spriteBatch.Draw(texture, new Rectangle((int)Position.X + Width, (int)Position.Y, lineWidth, Height + lineWidth), color);
+            spriteBatch.Draw(texture, new Rectangle((int)Position.X, (int)Position.Y + Height, Width + lineWidth, lineWidth), color);
+        }
+    }
+}
