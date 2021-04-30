297cfbb4dee35ab639d8b04a12da4757d224905e
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-04-30 08:01:57 -0300

    fix(GridBox): Bounds not being drawn when requested

--- UI/GridBox.cs
+++ UI/GridBox.cs
@@ -6,6 +6,8 @@ using System.Text;
 
 namespace Pirita.UI {
     public class GridBox {
+        private Texture2D _boundsTexture;
+
         public int Width;
         public int Height;
         public Vector2 Position;
@@ -17,14 +19,18 @@ namespace Pirita.UI {
         }
 
         public void RenderBounds(SpriteBatch spriteBatch) {
-            var texture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
-            var color = Color.Yellow;
+            if (_boundsTexture == null) {
+                _boundsTexture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+                _boundsTexture.SetData<Color>(new Color[] { Color.Yellow });
+            }
+
+            var color = Color.White;
             var lineWidth = 1;
 
-            spriteBatch.Draw(texture, new Rectangle((int) Position.X, (int) Position.Y, lineWidth, Height + lineWidth), color);
-            spriteBatch.Draw(texture, new Rectangle((int)Position.X, (int)Position.Y, Width + lineWidth, lineWidth), color);
-            spriteBatch.Draw(texture, new Rectangle((int)Position.X + Width, (int)Position.Y, lineWidth, Height + lineWidth), color);
-            spriteBatch.Draw(texture, new Rectangle((int)Position.X, (int)Position.Y + Height, Width + lineWidth, lineWidth), color);
+            spriteBatch.Draw(_boundsTexture, new Rectangle((int)Position.X, (int)Position.Y, lineWidth, Height + lineWidth), color);
+            spriteBatch.Draw(_boundsTexture, new Rectangle((int)Position.X, (int)Position.Y, Width + lineWidth, lineWidth), color);
+            spriteBatch.Draw(_boundsTexture, new Rectangle((int)Position.X + Width, (int)Position.Y, 1, Height + lineWidth), color);
+            spriteBatch.Draw(_boundsTexture, new Rectangle((int)Position.X, (int)Position.Y + Height, Width + lineWidth, lineWidth), color);
         }
     }
 }
