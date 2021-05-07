465ac39e3f4f4d6a86e30c03c5e251e657964cf9
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-05-07 19:46:25 -0300

    feat: Entity Component system classes

---
+++ EC/Component.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pirita.EC {
+    public abstract class Component {
+        public int zIndex;
+
+        public Entity Owner;
+
+        public bool Enabled = true;
+        public bool Visible = false;
+
+        public abstract void Initialize();
+        public abstract void Update(GameTime gameTime);
+        public abstract void PostUpdate(GameTime gameTime);
+        public abstract void Render(SpriteBatch spriteBatch);
+        public abstract void Destroy();
+    }
+}
