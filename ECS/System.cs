6c631509cabcbf69ce663ee8b5247174880ea24e
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-06-21 08:06:04 -0300

    feat(ECS): System base class

---
+++ ECS/System.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Pirita.ECS {
+    public class System {
+        public List<Type> RequiredComponents { get; private set; } = new List<Type>();
+
+        public bool HasRequiredComponents(Entity entity) {
+            foreach (var type in RequiredComponents) {
+                if (!entity.HasComponent(type)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public virtual void Update(GameTime gameTime, Entity entity) { }
+    }
+}
