079d5c0769f4b20b217083b3df3ee92329dcf78e
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-06-21 19:11:39 -0300

    refactor(Component): remove useless component data

--- ECS/Component.cs
+++ ECS/Component.cs
@@ -3,14 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Pirita.ECS {
     public abstract class Component {
-        public int Depth;
-
         public Entity Owner;
-
         public bool Enabled = true;
-        public bool Visible = false;
-
-        public abstract void Initialize();
-        public abstract void Destroy();
     }
 }
