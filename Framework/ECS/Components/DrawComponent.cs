38718273b00756245ec121239d11a4fab29d07c3
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-10-01 16:15:21 -0300

    feat(DrawComponent): add default value to opacity to 1f

--- Framework/ECS/Components/DrawComponent.cs
+++ Framework/ECS/Components/DrawComponent.cs
@@ -4,7 +4,7 @@ namespace Pirita.ECS;
 
 public class DrawComponent : Component {
     public int Depth;
-    public float Opacity { get; set; }
+    public float Opacity { get; set; } = 1f;
     public Vector2 Scale { get; set; } = Vector2.One;
     public float Rotation { get; set; }
     public Vector2 Origin { get; set; }
