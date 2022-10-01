ef9dfdab2f8442e814b3f5d80971e91102e320fd
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-10-01 15:54:13 -0300

    feat(PositionComponent): add construct methods

--- Framework/ECS/Components/PositionComponent.cs
+++ Framework/ECS/Components/PositionComponent.cs
@@ -5,6 +5,18 @@ namespace Pirita.ECS;
 public class PositionComponent : Component {
     public Vector2 Position { get; set; }
 
+    public PositionComponent() {
+        Position = Vector2.Zero;
+    }
+
+    public PositionComponent(Vector2 position) {
+        Position = position;
+    }
+
+    public PositionComponent(float x, float y) {
+        Position = new Vector2(x, y);
+    }
+
     public void Deconstruct(out Vector2 position) {
         position = Position;
     }
